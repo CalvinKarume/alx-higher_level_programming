84a4353f2e24bef6b44f94d58657dba6d79a39c8
Calvin <johncalvinkarume@gmail.com>
2023-06-30 15:11:33 -0700

    Ma 123 here and there

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Display the byte size of the HTTP response header for a given URL
-curl -s "$1" | wc -c
+[master feeee22] New
+ 1 file changed, 0 insertions(+), 0 deletions(-)
+ mode change 100644 => 100755 0x10-python-network_0/0-body_size.sh
